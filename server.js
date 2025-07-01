0bdfffb0427ff5427566579c619810eee06b4494
jayzo31 <jayzo21@gmail.com>
2025-07-01 11:34:48 -0400

    Refactor CORS to use explicit origin function

--- server.js
+++ server.js
@@ -28,9 +28,19 @@ app.use(helmet({
 
 // CORS configuration
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? [process.env.BACKEND_URL, process.env.FRONTEND_URL]
-    : ['http://localhost:3000', 'http://localhost:3001'],
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      process.env.BACKEND_URL,
+      process.env.FRONTEND_URL,
+      'http://localhost:3000',
+      'http://localhost:3001'
+    ];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
